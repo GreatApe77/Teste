d312450e6f847fdc01eee39e66650e6e5634556e
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2022-10-04 19:20:56 -0300

    a

--- index.py
+++ index.py
@@ -1,3 +1,14 @@
+from array import array
+from time import sleep
+
+from os import system
+
+
 Numberfiles = int(input("digite a quantidade de arquivos"))
 
+vetor = []
 
+for i in range(1,Numberfiles+1):
+    system(f"mkdir oi")
+    sleep(1)
+    system(f"rmdir oi")
